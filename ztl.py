52f49343ee0f49c86186c14222031220beab0c7f
Javier Santacruz <javier.santacruz.lc@gmail.com>
2016-03-30 21:38:34 +0200

    Adds first printing of the carrousel

--- ztl.py
+++ ztl.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import datetime
 
 import click
 
 
-@click.group()
+@click.command()
 def cli():
     """zone time line"""
+    utc = datetime.datetime.utcnow()
+    local = datetime.datetime.now()
+
+    print('UTC time: {}'.format(utc))
+    print('local time: {}'.format(local))
+    print()
+
+    print('UTC:        ', end='')
+    for h in range(24):
+        print('{:2d}   '.format((utc.hour + h) % 24), end='')
+    print()
+    print('local time: ', end='')
+    for h in range(24):
+        print('{:2d}   '.format((local.hour + h) % 24), end='')
+    print()
+
+
+    print()
 
 
 if __name__ == '__main__':

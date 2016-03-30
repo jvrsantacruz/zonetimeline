76b1eaf24a27a292aeaafd406f356e41c320f580
Javier Santacruz <javier.santacruz.lc@gmail.com>
2016-03-30 21:45:48 +0200

    Adds markers for timeline

--- ztl.py
+++ ztl.py
@@ -12,19 +12,35 @@ def cli():
     utc = datetime.datetime.utcnow()
     local = datetime.datetime.now()
 
-    print('UTC time: {}'.format(utc))
-    print('local time: {}'.format(local))
+    print(u'UTC time: {}'.format(utc))
+    print(u'local time: {}'.format(local))
     print()
 
-    print('UTC:        ', end='')
+    print(u'            ', end='')
     for h in range(24):
-        print('{:2d}   '.format((utc.hour + h) % 24), end='')
-    print()
-    print('local time: ', end='')
-    for h in range(24):
-        print('{:2d}   '.format((local.hour + h) % 24), end='')
+        if h == 0:
+            print(u'↓↓   ', end='')
+        else:
+            print(u'     ', end='')
     print()
 
+    print(u'UTC:        ', end='')
+    for h in range(24):
+        print(u'{:2d}   '.format((utc.hour + h) % 24), end='')
+    print()
+
+    print(u'local time: ', end='')
+    for h in range(24):
+        print(u'{:2d}   '.format((local.hour + h) % 24), end='')
+    print()
+
+    print(u'            ', end='')
+    for h in range(24):
+        if h == 0:
+            print(u'↑↑   ', end='')
+        else:
+            print(u'     ', end='')
+    print()
 
     print()
 
